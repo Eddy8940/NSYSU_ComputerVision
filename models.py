b1567b316cf25ee9b5ae57338118406b5cecd280
ImMasterSam <sam9630147@gmail.com>
2025-11-23 20:25:20 +0800

    Add LeNet-5

--- models.py
+++ models.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class Net(nn.Module):
-    def __init__(self, input_size=128*128, output_classes=10):
+    def __init__(self, input_size=128*128, output_classes=2):
         super().__init__()
         self.fc1 = nn.Linear(input_size, 512)
         self.layer1 = nn.Sequential(
@@ -20,5 +20,32 @@ class Net(nn.Module):
         x = F.relu(self.fc1(x))
         x = self.layer1(x)
         x = self.fc2(x)
+
+        return x
+    
+class LeNet_5(nn.Module):
+    def __init__(self, input_size=32*32, output_classes=2):
+        super().__init__()
+        self.cnn_model = nn.Sequential(
+            nn.Conv2d(1, 6, 5, padding=2), 
+            nn.Sigmoid(),       
+            nn.MaxPool2d(2, stride=2),  
+            nn.Conv2d(6, 16, 5, stride=1, padding=2),       
+            nn.ReLU(),
+            nn.MaxPool2d(2, stride=2)
+        )
+        self.fc_model = nn.Sequential(
+            nn.Linear(16*8*8, 120),         
+            nn.Sigmoid(),
+            nn.Linear(120, 84),          
+            nn.Sigmoid(),
+            nn.Linear(84, output_classes)            
+        )
+
+    def forward(self, x):
+        
+        x = self.cnn_model(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc_model(x)
 
         return x
